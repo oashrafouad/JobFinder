9db28c3c8a325ee18865ea5614daf592ae32e00f
TadrosNasr <tadrosnasr7@gmail.com>
2023-02-18 19:30:35 +0200

    pop profileVC to root VC when press log out

--- JobFinder/Controller/ProfileVC.swift
+++ JobFinder/Controller/ProfileVC.swift
@@ -13,8 +13,8 @@ class ProfileVC: UIViewController {
     @IBAction func logOutButtonPressed(_ sender: UIButton) {
         
         // Return back to login/register screen first time, then return to welcome screen second time
-        self.navigationController?.popViewController(animated: true)
-
+//        self.navigationController?.popViewController(animated: true)
+        self.navigationController?.popToRootViewController(animated: true)
 
         //self.navigationController?.popViewController(animated: true)
 
