9b507f54fef4a9c4c8b161c73cb360d1327f4086
oashrafouad <amourakora2001@gmail.com>
2023-02-24 14:50:05 +0200

    edit

---
+++ JobFinder/Controller/jobTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  jobTableViewCell.swift
+//  JobFinder
+//
+//  Created by Omar Ashraf on 24/02/2023.
+//
+
+import UIKit
+
+class jobTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
