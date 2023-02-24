f7475abadc385e2fab3cd37c250932bec81f9d34
oashrafouad <amourakora2001@gmail.com>
2023-02-24 14:53:19 +0200

    edit

--- JobFinder/Controller/JobTableViewCell.swift
+++ JobFinder/Controller/JobTableViewCell.swift
@@ -12,6 +12,7 @@ class JobTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        print("test")
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
